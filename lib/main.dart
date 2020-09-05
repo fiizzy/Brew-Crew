baf67e32ea3dd2db6c9d5c592fe33de5c9f73f69
obilaja@gmail.com <obilaja@gmail.com>
2020-09-05 07:23:25 +0100

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  
+  @override
+ Widget build(BuildContext context) {
+   return (
+     new MaterialApp(
+       home: Login(),
+       title: 'Firebase Login',
+       theme: ThemeData.dark(),
+       
+     )
+   );
+ }
+}
